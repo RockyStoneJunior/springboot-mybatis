7511d9ed4aad6c28cb31521bb2e37f64335df82c
Administrator <511697856@qq.com>
2018-05-30 17:45:23 +0800

    add self defined annotation

---
+++ src/main/java/cn/no7player/annotation/MyLog.java
@@ -0,0 +1,18 @@
+package cn.no7player.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author sam
+ * @since 2017/7/13
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface MyLog {
+
+    String requestUrl();
+
+}
--- src/main/java/cn/no7player/controller/LoginController.java
+++ src/main/java/cn/no7player/controller/LoginController.java
@@ -2,12 +2,20 @@ package cn.no7player.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
+import cn.no7player.annotation.MyLog;
 import cn.no7player.model.User;
+import cn.no7player.model.UserDto;
 import cn.no7player.service.UserService;
 
+@Controller
 public class LoginController {
 	private Logger logger = Logger.getLogger(LoginController.class);
 
@@ -16,12 +24,21 @@ public class LoginController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public User getUserInfo(String username) {
+    public User findUserByName(String username) {
         User user = userService.findUserByName(username);
         if(user!=null){
             System.out.println("user.getName():"+user.getName());
             logger.info("user.getAge():"+user.getAge());
         }
         return user;
+    }
+    
+    @MyLog(requestUrl = "/user/registration")
+    @RequestMapping(method = RequestMethod.GET)
+    //@RequestMapping(value = "/user/registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
     }
 }
